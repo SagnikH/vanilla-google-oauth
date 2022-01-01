b9ef917ddecd474a542a087df76b07945e459a65
Sagnik Haldar <sagnikh2@gmail.com>
2022-01-01 18:16:45 +0530

    try catch implemented

--- client/app.js
+++ client/app.js
@@ -1,10 +1,20 @@
-function getData() {
-    // fetch("http://localhost:4000/api/user", {
-    //     credentials: 'include'
-    // }).then(data => {
-    //     document.getElementById('info').innerHTML = JSON.stringify(data);
-    // })
-    axios.get("http://localhost:4000/api/user", {
-        withCredentails: true
-    }).then(data => { document.getElementById('info').innerHTML = data; }).catch(err => console.log)
+async function getData() {
+	// fetch("http://localhost:4000/api/user", {
+	//     credentials: 'include'
+	// }).then(data => {
+	//     document.getElementById('info').innerHTML = JSON.stringify(data);
+	// })
+	// axios.get("http://localhost:4000/api/user", {
+	//     withCredentails: true
+	// }).then(data => { document.getElementById('info').innerHTML = data; }).catch(err => console.log)
+
+	try {
+		const data = await axios.get("http://localhost:4000/api/user", {
+			withCredentials: true,
+		});
+		console.log("got the data", data);
+		document.getElementById("info").innerHTML = JSON.stringify(data);
+	} catch (e) {
+		console.error(e);
+	}
 }
